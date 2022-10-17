9d0cc964c0aaa5f85ddca66365025dc8ef739f4b
TGropper <TGropper@IPC6275.inconso.de>
2022-10-17 16:24:13 +0200

    Grundgerüst der Converter-Klasse

---
+++ chessProject/src/gamePackage/Converter.java
@@ -0,0 +1,56 @@
+package gamePackage;
+
+import java.util.HashMap;
+
+/**
+ * Dient als Funktionssammlung verschiedener Converter-Methoden
+ * 
+ * @comment Grundgerüst
+ * @author Toni Gropper
+ * @since 17.10.2022
+ */
+public class Converter {
+
+  /**
+   * konvertiert HashMap mit Objekten in einen String der FEN-Notation
+   * 
+   * @param HashMap
+   * @return String
+   * @throws
+   * @comment Grundgerüst
+   * @author Toni Gropper
+   */
+  public HashMap<String, Object> convertHashToFEN() {
+
+    return null;
+
+  }
+
+  /**
+   * konvertiert den FEN-String in eine HashMap
+   * 
+   * @param String
+   * @return HashMap
+   * @throws
+   * @comment Grundgerüst
+   * @author Toni Gropper
+   */
+  public String convertFENToHash() {
+
+    return null;
+  }
+
+  /**
+   * konvertiert FEN-String in einen History-String
+   * 
+   * @param String
+   * @return String
+   * @throws
+   * @comment Grundgerüst
+   * @author Toni Gropper
+   */
+  public String convertFENToHistory() {
+
+    return null;
+  }
+}

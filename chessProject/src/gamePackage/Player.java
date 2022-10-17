d56087ccf06fb017e3d44a4b6ed5297141147490
TGropper <TGropper@IPC6275.inconso.de>
2022-10-17 16:24:50 +0200

    grundgerüst der Player-Klasse

---
+++ chessProject/src/gamePackage/Player.java
@@ -0,0 +1,57 @@
+package gamePackage;
+
+import java.util.ArrayList;
+
+/**
+ * Representiert den Spieler mit seinem Namen, Farbe, Punktestand und den
+ * geschlagenen gegnerischen Figuren
+ * 
+ * @comment Grundgerüst, statt public Object sollte public Piece stehen
+ * @author Toni Gropper
+ * @since 17.10.2022
+ */
+public class Player {
+  private String               name;
+  private char                 colour;
+  private int                  score;
+  private ArrayList<Character> takenPiece;
+  /**
+   * fügt this.takenPiece das geschlagene Piece hinzu und ruft increaseScore auf
+   * 
+   * @param Object Piece
+   * @return void
+   * @throws
+   * @comment Grundgerüst
+   * @author Toni Gropper
+   */
+  public Object addTakenPiece() {
+    return null;
+  }
+
+  /**
+   * this.score + int; der int-Wert entspricht dem Piece-Wert
+   * 
+   * @param int
+   * @return void
+   * @throws
+   * @comment Grundgerüst
+   * @author Toni Gropper
+   */
+  public int increaseScore() {
+    return 0;
+  }
+
+  /**
+   * setzt alle Attribute auf Initialisierungswerte zurück
+   * 
+   * @param void
+   * @return void
+   * @throws
+   * @comment Grundgerüst
+   * @author Toni Gropper
+   */
+  public void setToInit() {
+
+  }
+
+}

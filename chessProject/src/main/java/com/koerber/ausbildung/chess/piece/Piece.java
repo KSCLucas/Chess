3d49fe028ab48c082d84c6f03f9a1da576594b5d
PKamps <PKamps@IPC6431.inconso.de>
2022-10-24 13:55:26 +0200

    Add reworked createLegalMoveMap method + initialise Piece fields in dec

--- chessProject/src/main/java/com/koerber/ausbildung/chess/piece/Piece.java
+++ chessProject/src/main/java/com/koerber/ausbildung/chess/piece/Piece.java
@@ -3,6 +3,8 @@ package com.koerber.ausbildung.chess.piece;
 import java.awt.Image;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import com.koerber.ausbildung.chess.Field;
 import com.koerber.ausbildung.chess.utility.PieceOutOfBoundsException;
@@ -11,61 +13,51 @@ import com.koerber.ausbildung.chess.utility.PieceOutOfBoundsException;
  * The {@code Piece} class contains a constructor as well as methods for every
  * {@code Piece}.
  * 
- * @comment The {@code legalMoveMap} is built parallel to the constructor.
  * @author PKamps
  */
 public abstract class Piece {
 
-  public static final String            TRUE_STRING  = "ttt";
-  public static final String            FALSE_STRING = "fff";
-  public static final String            HIT_STRING   = "hhh";
-  private String                        name;
-  private char                          colour;
-  private int                           value;
-  private boolean                       isMoveRepeatable;
-  private String                        position;
-  private ArrayList<ArrayList<Integer>> moveSet;
-  private Image                         skin;
-  private HashMap<String, String>       legalMoveMap;
+  public static final String       TRUE_STRING  = "ttt";
+  public static final String       FALSE_STRING = "fff";
+  public static final String       HIT_STRING   = "hhh";
+  private String                   id;
+  private char                     colour;
+  private int                      value;
+  private boolean                  isMoveRepeatable;
+  private String                   position;
+  private List<ArrayList<Integer>> moveSet;
+  private Image                    icon;
+  private Map<String, String>      legalMoveMap = new HashMap<String, String>();
 
   /**
    * Parameterized constructor for a {@code Piece}.
    * 
-   * @param name
+   * @param id
    * @param colour
    * @param value
    * @param isMoveRepeatable
    * @param position
    * @param moveSet
-   * @param skin
+   * @param icon
    * @author PKamps
    */
-  public Piece(String name, char colour, int value, boolean isMoveRepeatable, String position,
-      ArrayList<ArrayList<Integer>> moveSet, Image skin) {
-    this.name = name;
+  public Piece(String id, char colour, int value, boolean isMoveRepeatable, String position,
+      List<ArrayList<Integer>> moveSet, Image icon) {
+    this.id = id;
     this.colour = colour;
     this.value = value;
     this.isMoveRepeatable = isMoveRepeatable;
     this.position = position;
     this.moveSet = moveSet;
-    this.skin = skin;
-    this.legalMoveMap = new HashMap<String, String>();
-
-    // Fill HashMap with default values
-
-    for(int i = (int)Field.LEFT_BOUND; i <= (int)Field.RIGHT_BOUND; i++) {
-      for(int j = Field.LOWER_BOUND; j <= Field.UPPER_BOUND; j++) {
-        getLegalMoveMap().putIfAbsent(Character.toString(i) + String.valueOf(j), FALSE_STRING);
-      }
-    }
+    this.icon = icon;
   }
 
-  public String getName() {
-    return name;
+  public String getId() {
+    return id;
   }
 
-  public void setName(String name) {
-    this.name = name;
+  public void setId(String id) {
+    this.id = id;
   }
 
   public char getColour() {
@@ -92,23 +84,23 @@ public abstract class Piece {
     this.position = position;
   }
 
-  public ArrayList<ArrayList<Integer>> getMoveSet() {
+  public List<ArrayList<Integer>> getMoveSet() {
     return moveSet;
   }
 
-  public Image getSkin() {
-    return skin;
+  public Image getIcon() {
+    return icon;
   }
 
-  public void setSkin(Image skin) {
-    this.skin = skin;
+  public void setIcon(Image icon) {
+    this.icon = icon;
   }
 
-  public HashMap<String, String> getLegalMoveMap() {
+  public Map<String, String> getLegalMoveMap() {
     return legalMoveMap;
   }
 
-  public void setLegalMoveMap(HashMap<String, String> legalMoveMap) {
+  public void setLegalMoveMap(Map<String, String> legalMoveMap) {
     this.legalMoveMap = legalMoveMap;
   }
 
@@ -137,40 +129,44 @@ public abstract class Piece {
    * @throws PieceOutOfBoundsException
    * @author PKamps
    */
-  public void createLegalMoveMap(HashMap<String, Object> currentGameState) throws PieceOutOfBoundsException {
-    boolean repeatLoop;
-    // Set contents of legalMoveMap to FALSE_STRING
-    for(int i = Field.LEFT_BOUND; i <= Field.RIGHT_BOUND; i++) {
-      for(int j = Field.LOWER_BOUND; j <= Field.UPPER_BOUND; j++) {
-        getLegalMoveMap().put(Character.toString(i) + String.valueOf(j), FALSE_STRING);
-      }
-    }
+  public void createLegalMoveMap(Map<String, Piece> currentGameState) throws PieceOutOfBoundsException {
+    // Clear legalMoveMap
+    getLegalMoveMap().clear();
     // Loop over every move vector in moveSet
-    for(int i = 0; i < getMoveSet().size(); i++) {
-      repeatLoop = true;
+    for(ArrayList<Integer> moveVector : getMoveSet()) {
       int posLetterAsNumber = getPosition().charAt(0);
-      int posNumber = getPosition().charAt(1);
+      int posNumber = Character.getNumericValue(getPosition().charAt(1));
+      if(posLetterAsNumber < Field.LEFT_BOUND || posLetterAsNumber > Field.RIGHT_BOUND || posNumber < Field.LOWER_BOUND
+          || posNumber > Field.UPPER_BOUND) {
+        throw new PieceOutOfBoundsException();
+      }
       // Change content of legalMoveMap based on move vector i and
       // currentGameState
+      boolean repeatLoop = true;
       do {
-        posLetterAsNumber += getMoveSet().get(i).get(0);
-        posNumber += getMoveSet().get(i).get(1);
-        String str = Character.toString(posLetterAsNumber) + String.valueOf(posNumber);
-        try {
-          if(currentGameState.get(str).equals(Piece.class)) {
-            getLegalMoveMap().put(str, HIT_STRING);
+        posLetterAsNumber += moveVector.get(0);
+        posNumber += moveVector.get(1);
+        String fieldKey = Character.toString(posLetterAsNumber) + posNumber;
+        // Check for fieldKey still on Field
+        if(posLetterAsNumber >= Field.LEFT_BOUND && posLetterAsNumber <= Field.RIGHT_BOUND
+            && posNumber >= Field.LOWER_BOUND && posNumber <= Field.UPPER_BOUND) {
+          // Check for EmptyPiece
+          if(currentGameState.get(fieldKey).getId().equals("###")) {
+            getLegalMoveMap().put(fieldKey, TRUE_STRING);
+          }
+          // Check for opposing Piece
+          else if(currentGameState.get(fieldKey).getColour() != getColour()) {
+            getLegalMoveMap().put(fieldKey, HIT_STRING);
             repeatLoop = false;
           }
           else {
-            getLegalMoveMap().put(str, TRUE_STRING);
+            repeatLoop = false;
           }
         }
-        catch(Exception e) {
-          throw new PieceOutOfBoundsException();
+        else {
+          repeatLoop = false;
         }
-      } while(posLetterAsNumber >= Field.LEFT_BOUND && posLetterAsNumber <= Field.RIGHT_BOUND
-          && posNumber >= Field.LOWER_BOUND && posNumber <= Field.UPPER_BOUND && isMoveRepeatable() && repeatLoop);
+      } while(isMoveRepeatable() && repeatLoop);
     }
   }
-
 }

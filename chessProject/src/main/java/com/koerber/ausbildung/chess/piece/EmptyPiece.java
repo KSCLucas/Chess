7dbed68575e05365e12fc50eb729879da01c08ce
PKamps <PKamps@IPC6431.inconso.de>
2022-11-03 09:51:28 +0100

    Refactor EmptyPiece

--- chessProject/src/main/java/com/koerber/ausbildung/chess/piece/EmptyPiece.java
+++ chessProject/src/main/java/com/koerber/ausbildung/chess/piece/EmptyPiece.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.swing.ImageIcon;
 
 import com.koerber.ausbildung.chess.utility.ChessColour;
+import com.koerber.ausbildung.chess.utility.MoveVector;
 
 /**
  * The {@code EmptyPiece} class provides a standardised constructor for every
@@ -17,13 +18,13 @@ import com.koerber.ausbildung.chess.utility.ChessColour;
  */
 public class EmptyPiece extends Piece {
 
-  public static final String                   ID                 = "###";
-  public static final ChessColour              COLOUR             = ChessColour.NONE;
-  public static final int                      VALUE              = 0;
-  public static final boolean                  IS_MOVE_REPEATABLE = false;
-  public static final String                   POSITION           = "nd";
-  public static final List<ArrayList<Integer>> MOVESET            = new ArrayList<>();
-  public static final ImageIcon                ICON               = null;
+  public static final String           ID                 = "###";
+  public static final ChessColour      COLOUR             = ChessColour.NONE;
+  public static final int              VALUE              = 0;
+  public static final boolean          IS_MOVE_REPEATABLE = false;
+  public static final String           POSITION           = "nd";
+  public static final List<MoveVector> MOVESET            = new ArrayList<>();
+  public static final ImageIcon        ICON               = null;
 
   /**
    * Calls parameterized constructor of {@code Piece} and passes values for

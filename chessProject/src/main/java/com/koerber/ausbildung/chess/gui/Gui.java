f4877ef4a4e3e07c8111c643261591330144f41d
KSCLucas <115541009+KSCLucas@users.noreply.github.com>
2022-11-04 13:30:28 +0100

    Refactor gui frame and gui
    Moved highlightLegalMove and showCurrentGame to Gui.
    Moved setGridBag to GuiUtility

--- chessProject/src/main/java/com/koerber/ausbildung/chess/gui/Gui.java
+++ chessProject/src/main/java/com/koerber/ausbildung/chess/gui/Gui.java
@@ -1,5 +1,23 @@
 package com.koerber.ausbildung.chess.gui;
 
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.Map.Entry;
+
+import javax.swing.JLabel;
+
+import com.koerber.ausbildung.chess.Field;
+import com.koerber.ausbildung.chess.piece.King;
+import com.koerber.ausbildung.chess.piece.Knight;
+import com.koerber.ausbildung.chess.piece.Piece;
+import com.koerber.ausbildung.chess.piece.Rook;
+import com.koerber.ausbildung.chess.utility.ChessColour;
+import com.koerber.ausbildung.chess.utility.PieceOutOfBoundsException;
+
 /**
  * GUI class provides the entire GUI. It communicates with the Field & Player
  * class.
@@ -29,6 +47,7 @@ public class Gui {
 
   /**
    * Converts HashMap Keyvalue to LabelArray Index.
+   * 
    * @param columnValue
    * @param rowValue
    * @return
@@ -117,11 +136,182 @@ public class Gui {
   }
 
   /**
+   * Dispays {@code currentGameState} on chessboard.
+   * 
+   * @return
+   */
+  public static JLabel[] showCurrentGameState() {
+    // create test map start
+    // TODO:remove test
+    Knight tempKnight = new Knight("n1b", ChessColour.BLACK, "D4");
+    Rook tempRook = new Rook("n1w", ChessColour.WHITE, "F5", 'l');
+    Rook tempRook1 = new Rook("n1w", ChessColour.WHITE, "F3", 'l');
+    King tempKing = new King("k1b", ChessColour.BLACK, "B7");
+
+    Map<String, Piece> currentGameStateTemp = new TreeMap<String, Piece>();
+    for(int i = Field.LEFT_BOUND; i <= Field.RIGHT_BOUND; i++) {
+      for(int j = Field.LOWER_BOUND; j <= Field.UPPER_BOUND; j++) {
+        currentGameStateTemp.put(Character.toString(i) + String.valueOf(j), null);
+      }
+    }
+    currentGameStateTemp.put("D4", tempKnight);
+    currentGameStateTemp.put("F5", tempRook);
+    currentGameStateTemp.put("F3", tempRook1);
+    currentGameStateTemp.put("B7", tempKing);
+    // test end
+
+    JLabel[] currentGameStateLabels = new JLabel[64];
+    MouseListener m1 = new MouseListener() {
+      JLabel       lastEntered    = new JLabel();
+      JLabel       tempImageLabel = new JLabel();
+      List<JLabel> deleteLabels   = new ArrayList<>();
+      @Override
+      public void mouseClicked(MouseEvent e) {
+
+      }
+
+      @Override
+      public void mousePressed(MouseEvent e) {
+        System.out.println(e.getComponent().getName());
+        tempImageLabel = (JLabel)e.getComponent();
+
+        deleteLabels.add(((JLabel)e.getComponent()));
+
+        // TODO Auto-generated method stub
+      }
+
+      @Override
+      public void mouseReleased(MouseEvent e) {
+        System.out.println(lastEntered.getName());
+        lastEntered = (JLabel)e.getComponent();
+        ((JLabel)e.getComponent()).setIcon(tempImageLabel.getIcon());
+
+        for(int i = 0; i < deleteLabels.size() - 1; i++) {
+          deleteLabels.get(i).setIcon(null);
+        }
+        // TODO Auto-generated method stub
+      }
+
+      @Override
+      public void mouseEntered(MouseEvent e) {
+        lastEntered = (JLabel)e.getComponent();
+
+        if(tempImageLabel.getIcon() != null) {
+          ((JLabel)e.getComponent()).setIcon(tempImageLabel.getIcon());
+        }
+        deleteLabels.add(((JLabel)e.getComponent()));
+        // check if still JLabel (if (instanceoff))
+        // TODO Auto-generated method stub
+      }
+
+      @Override
+      public void mouseExited(MouseEvent e) {
+        // ((JLabel)e.getComponent()).setIcon(null);
+        // TODO Auto-generated method stub
+      }
+
+    };
+    List<String> xAxis = new ArrayList<String>();
+    xAxis.add("A");
+    xAxis.add("B");
+    xAxis.add("C");
+    xAxis.add("D");
+    xAxis.add("E");
+    xAxis.add("F");
+    xAxis.add("G");
+    xAxis.add("H");
+    for(int i = 0; i < 64; i++) {
+      currentGameStateLabels[i] = new JLabel();
+      currentGameStateLabels[i].addMouseListener(m1);
+      // currentGameStateLabels[i].setOpaque(true);
+      if(i >= 0 && i < 8) {
+        currentGameStateLabels[i].setName(xAxis.get(i) + 8);
+      }
+      if(i >= 8 && i < 16) {
+        currentGameStateLabels[i].setName(xAxis.get(i - 8) + 7);
+      }
+      if(i >= 16 && i < 24) {
+        currentGameStateLabels[i].setName(xAxis.get(i - 16) + 6);
+      }
+      if(i >= 24 && i < 32) {
+        currentGameStateLabels[i].setName(xAxis.get(i - 24) + 5);
+      }
+      if(i >= 32 && i < 40) {
+        currentGameStateLabels[i].setName(xAxis.get(i - 32) + 4);
+      }
+      if(i >= 40 && i < 48) {
+        currentGameStateLabels[i].setName(xAxis.get(i - 40) + 3);
+      }
+      if(i >= 48 && i < 56) {
+        currentGameStateLabels[i].setName(xAxis.get(i - 48) + 2);
+      }
+      if(i >= 56 && i < 64) {
+        currentGameStateLabels[i].setName(xAxis.get(i - 56) + 1);
+      }
+
+    }
+
+    for(Entry<String, Piece> entry : currentGameStateTemp.entrySet()) {
+      if(entry.getValue() != null) {
+        int columnAsNumber = entry.getKey().charAt(0) - 64;
+        int rowAsNumber = entry.getKey().charAt(1) - 48;
+        currentGameStateLabels[Gui.getIndex(columnAsNumber, rowAsNumber)].setIcon(entry.getValue().getIcon());
+      }
+
+    }
+    return currentGameStateLabels;
+
+  }
+
+  /**
    * Colors the fields according to the {@code Piece.legalMoveMap} green (may
    * move), red (hit) or not at all (may not move).
+   * 
+   * @return
    */
-  public void highlightLegalMove() {
+  public static JLabel[] highlightLegalMove(/* legalMoveMap */) {
+    // TODO remove example
+    Knight tempKnight = new Knight("n1b", ChessColour.BLACK, "D4");
+    Rook tempRook = new Rook("n1w", ChessColour.WHITE, "F5", 'l');
+    Rook tempRook1 = new Rook("n1w", ChessColour.WHITE, "F3", 'l');
+    Map<String, Piece> currentGameStateTemp = new TreeMap<String, Piece>();
+    for(int i = Field.LEFT_BOUND; i <= Field.RIGHT_BOUND; i++) {
+      for(int j = Field.LOWER_BOUND; j <= Field.UPPER_BOUND; j++) {
+        currentGameStateTemp.put(Character.toString(i) + String.valueOf(j), null);
+      }
+    }
+    currentGameStateTemp.put("D4", tempKnight);
+    currentGameStateTemp.put("F5", tempRook);
+    currentGameStateTemp.put("F3", tempRook1);
+    try {
+      tempKnight.createLegalMoveMap(currentGameStateTemp);
+    }
+    catch(PieceOutOfBoundsException e) {
+      e.printStackTrace();
+    }
+    // functional code
+    Map<String, String> legalMoveMapTemp = new TreeMap<>(tempKnight.getLegalMoveMap());
+    JLabel[] legalMoveLabels = new JLabel[64];
+    for(int i = 0; i < 64; i++) {
+      legalMoveLabels[i] = new JLabel();
+      legalMoveLabels[i].setText("");
+    }
 
+    for(Map.Entry<String, String> entry : legalMoveMapTemp.entrySet()) {
+      if(entry.getValue().equals(Piece.TRUE_STRING)) {
+        int columnAsNumber = entry.getKey().charAt(0) - 64;
+        int rowAsNumber = entry.getKey().charAt(1) - 48;
+        legalMoveLabels[Gui.getIndex(columnAsNumber, rowAsNumber)].setOpaque(true);
+        legalMoveLabels[Gui.getIndex(columnAsNumber, rowAsNumber)].setBackground(GuiFrame.LIGHT_GREEN);
+      }
+      if(entry.getValue().equals(Piece.HIT_STRING)) {
+        int columnAsNumber = entry.getKey().charAt(0) - 64;
+        int rowAsNumber = entry.getKey().charAt(1) - 48;
+        legalMoveLabels[Gui.getIndex(columnAsNumber, rowAsNumber)].setOpaque(true);
+        legalMoveLabels[Gui.getIndex(columnAsNumber, rowAsNumber)].setBackground(GuiFrame.LIGHT_RED);
+      }
+    }
+    return legalMoveLabels;
   }
 
   /**

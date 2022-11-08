0333c8a1042548095faf577bfe42450799cfeb90
PKamps <PKamps@IPC6431.inconso.de>
2022-11-08 08:57:36 +0100

    Refactor field

--- chessProject/src/main/java/com/koerber/ausbildung/chess/Field.java
+++ chessProject/src/main/java/com/koerber/ausbildung/chess/Field.java
@@ -5,20 +5,18 @@ import java.util.TreeMap;
 /**
  * Serves as a gameclass that manages everything related to chess.
  * 
- * @comment base-structure, currently final not initialized.
- * @author Toni Gropper
+ * @author Toni Gropper, PKamps
  * @since 19.10.2022
  */
 public class Field {
-  
-  public static final int UPPER_BOUND = 8;
-  public static final int LOWER_BOUND = 1;
-  public static final char LEFT_BOUND = 'A';
-  public static final char RIGHT_BOUND = 'H';
-  private TreeMap<String, Object>       currentGameState;
-  private int                           currentTurn;
-  private char                          whoWinner;
 
+  public static final int         UPPER_BOUND = 8;
+  public static final int         LOWER_BOUND = 1;
+  public static final char        LEFT_BOUND  = 'A';
+  public static final char        RIGHT_BOUND = 'H';
+  private TreeMap<String, Object> currentGameState;
+  private int                     currentTurn;
+  private char                    whoWinner;
 
   public TreeMap<String, Object> getCurrentGameState() {
     return currentGameState;
@@ -45,82 +43,47 @@ public class Field {
   }
 
   /**
-   * sets game pieces in this.currentGameState to the starting positions of
-   * this.initialGameState.
-   * 
-   * @param void
-   * @return void
-   * @throws
-   * @comment base-structure
-   * @author Toni Gropper
-   */
+   * Sets game pieces in this.currentGameState to the starting positions of
+   * {@code initialGameState}.
+  */
   public void initializeHashmap() {
 
   }
 
   /**
-   * updates this.currentGameState with the completed move.
-   * 
-   * @param void
-   * @return void
-   * @throws
-   * @comment base-structure
-   * @author Toni Gropper
+   * Updates {@code currentGameState} with the completed move.
    */
   public void updateHashMap() {
 
   }
 
   /**
-   * sets this.currentTurn to initial value (=1).
-   * 
-   * @param void
-   * @return void
-   * @throws
-   * @comment base-structure
-   * @author Toni Gropper
+   * Sets {@code currentTurn} to initial value (=1).
    */
   public void resetCurrentTurn() {
 
   }
 
   /**
-   * increases this.currentTurn by 1
-   * 
-   * @param void
-   * @return void
-   * @throws
-   * @comment base-structure
-   * @author Toni Gropper
+   * Increases {@code currentTurn} by 1.
    */
   public void increaseCurrentTurn() {
 
   }
-
+  
+  // TODO game loop needed???
   /**
-   * regulates the course of the chess game and calls all methods that are
+   * Regulates the course of the chess game and calls all methods that are
    * required for the game.
-   * 
-   * @param void
-   * @return void
-   * @throws
-   * @comment base-structure
-   * @author Toni Gropper
    */
   public void gameLoop() {
 
   }
 
   /**
-   * checks whether either player has won and sets this.whoWinner to 'w' or 'b';
-   * calls the GUI method showWinnerPopup. If none of the Kings are in checkmate
-   * then this.whoWinner ='n' (n=none) is set.
-   * 
-   * @param void
-   * @return void
-   * @throws
-   * @comment base-structure
-   * @author Toni Gropper
+   * Checks, if a player has won and sets {@code whoWinner} to {@code ChessColour.WHITE} or {@code ChessColour.BLACK},
+   * calls the GUI method {@code showWinnerPopup}. If none of the Kings are in checkmate
+   * then {@code whoWinner = ChessColour.NONE} is set.
    */
   public void checkForWinner() {
 

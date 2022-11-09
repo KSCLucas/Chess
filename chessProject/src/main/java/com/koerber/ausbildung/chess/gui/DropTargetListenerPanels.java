844b331f41985e6cb43b9c9d2c3b3837dc006122
KSCLucas <115541009+KSCLucas@users.noreply.github.com>
2022-11-09 09:16:09 +0100

    Add drop target listener

---
+++ chessProject/src/main/java/com/koerber/ausbildung/chess/gui/DropTargetListenerPanels.java
@@ -0,0 +1,26 @@
+package com.koerber.ausbildung.chess.gui;
+
+import java.awt.dnd.DnDConstants;
+import java.awt.dnd.DropTarget;
+import java.awt.dnd.DropTargetAdapter;
+import java.awt.dnd.DropTargetDropEvent;
+
+import javax.swing.JPanel;
+
+public class DropTargetListenerPanels extends DropTargetAdapter {
+
+  private DropTarget dropTarget;
+  private JPanel     p;
+
+  public DropTargetListenerPanels(JPanel p) {
+    this.p = p;
+    dropTarget = new DropTarget(p, DnDConstants.ACTION_COPY, this, true, null);
+  }
+
+  @Override
+  public void drop(DropTargetDropEvent dtde) {
+    // TODO Auto-generated method stub
+
+  }
+
+}

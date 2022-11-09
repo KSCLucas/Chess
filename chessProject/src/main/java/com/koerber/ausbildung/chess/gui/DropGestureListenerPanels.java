364bf8aaffa72510e4742201e7f5467b891ca00d
KSCLucas <115541009+KSCLucas@users.noreply.github.com>
2022-11-09 09:15:51 +0100

    Add drop gesture listener class

---
+++ chessProject/src/main/java/com/koerber/ausbildung/chess/gui/DropGestureListenerPanels.java
@@ -0,0 +1,43 @@
+package com.koerber.ausbildung.chess.gui;
+
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.awt.dnd.DragGestureEvent;
+import java.awt.dnd.DragGestureListener;
+import java.io.IOException;
+
+import javax.swing.Icon;
+import javax.swing.JLabel;
+
+public class DropGestureListenerPanels implements DragGestureListener {
+
+  @Override
+  public void dragGestureRecognized(DragGestureEvent event) {
+    JLabel label = (JLabel)event.getComponent();
+    Icon icon = label.getIcon();
+
+    Transferable transferable = new Transferable() {
+
+      @Override
+      public DataFlavor[] getTransferDataFlavors() {
+        return new DataFlavor[]{DataFlavor.imageFlavor};
+      }
+
+      @Override
+      public Object getTransferData(DataFlavor flavor) throws UnsupportedFlavorException, IOException {
+        return icon;
+      }
+
+      @Override
+      public boolean isDataFlavorSupported(DataFlavor flavor) {
+        if(!isDataFlavorSupported(flavor)) {
+          return false;
+        }
+        return true;
+      }
+    };
+
+  }
+
+}

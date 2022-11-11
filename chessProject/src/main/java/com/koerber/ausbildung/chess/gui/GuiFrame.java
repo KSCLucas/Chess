ef60a6be2bf48fd1a7e8d41667caddc93527ede0
PKamps <PKamps@IPC6431.inconso.de>
2022-11-11 12:45:19 +0100

    Refactor GuiFrame

--- chessProject/src/main/java/com/koerber/ausbildung/chess/gui/GuiFrame.java
+++ chessProject/src/main/java/com/koerber/ausbildung/chess/gui/GuiFrame.java
@@ -16,7 +16,6 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;

85cbb9675193e646c7a5c90eae1464de3decfce9
KSCLucas <115541009+KSCLucas@users.noreply.github.com>
2022-12-01 10:06:17 +0100

    Refactor main

--- chessProject/src/main/java/com/koerber/ausbildung/chess/Main.java
+++ chessProject/src/main/java/com/koerber/ausbildung/chess/Main.java
@@ -1,7 +1,5 @@
 package com.koerber.ausbildung.chess;
 
-
-
 public class Main {
 
   public static void main(String[] args) {
@@ -9,11 +7,7 @@ public class Main {
     System.out.println("Hello, Goodbye");
     System.out.println(" O haha reingeschaut");
     System.out.println(" schwanz");
-    History myHistory = new History();
-    myHistory.addEntry("fen");
-    System.out.println(myHistory.getFens());
-    System.out.println(myHistory.getFenOfTurn(0));
-    
+
   }
 
 }

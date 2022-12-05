217b9c229e8806b66ce104ffba668854529570cb
PKamps <PKamps@IPC6431.inconso.de>
2022-12-05 15:10:15 +0100

    Add Main method implementation

--- chessProject/src/main/java/com/koerber/ausbildung/chess/Main.java
+++ chessProject/src/main/java/com/koerber/ausbildung/chess/Main.java
@@ -1,13 +1,35 @@
 package com.koerber.ausbildung.chess;
 
+import java.awt.EventQueue;
+
+import com.koerber.ausbildung.chess.gui.GuiFrame;
+import com.koerber.ausbildung.chess.piece.King;
+import com.koerber.ausbildung.chess.utility.ChessColour;
+
 public class Main {
 
   public static void main(String[] args) {
-
-    System.out.println("Hello, Goodbye");
-    System.out.println(" O haha reingeschaut");
-    System.out.println(" schwanz");
-
+    Field field = new Field();
+    History history = new History();
+    field.initializeMap();
+    field.turnLock();
+    field.getCurrentGameState().entrySet().stream()
+        .filter(x -> x.getValue() instanceof King && x.getValue().getColour() == ChessColour.WHITE).forEach(x -> {
+          King king = (King)x.getValue();
+          king.checkForCheckAndCreateLegalMoveMap(field.getCurrentGameState());
+        });
+    EventQueue.invokeLater(new Runnable() {
+      public void run() {
+        try {
+          GuiFrame window = new GuiFrame(field, history);
+          window.getFrame().setVisible(true);
+          window.highlightActivePlayer(field, window.getPlayer1Label(), window.getPlayer2Label());
+        }
+        catch(Exception e) {
+          e.printStackTrace();
+        }
+      }
+    });
   }
 
 }
